62a3611495ea7027fb21a222714c17c2af1b045d
Syed-Asraar <syed.asraar5103@gmail.com>
2024-10-23 01:57:31 +0530

    Error solved

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const mongoose=require('mongoose');
-const connect=mongoose.connect("mongodb+srv://syedasraar5103:asraar@11@cluster0.zdd4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+const connect = mongoose.connect("mongodb+srv://syedasraar5103:asraar%4011@cluster0.zdd4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 connect.then(()=>{
     console.log("Database connected");
 })
-.catch(()=>{
-    console.log("Not connected");
+.catch((e)=>{
+    console.log("Not connected"+e);
 });
 const LoginSchema = new mongoose.Schema({
     name:{
